c821fe20d347423cae6b5232268cbdc1aeee7278
belal <belal@wizely.in>
2020-12-05 14:43:11 +0530

    Classes and Objects

---
+++ 2 ObjectOrientedProgramming/1 Classes and Objects/Box.kt
@@ -0,0 +1,15 @@
+class Box {
+
+    var length: Int = 10
+    var width: Int = 20
+    var height: Int = 5
+
+    fun fillContents(){
+        println("Box is Filled")
+    }
+
+    fun open(){
+        println("Box Opened")
+    }
+
+}
---
+++ 2 ObjectOrientedProgramming/1 Classes and Objects/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val box1 = Box()
+    val box2 = Box()
+    val box3 = Box()
+
+    box1.height = 7
+    println("Height: ${box1.height}")
+    println("Length: ${box1.length}")
+    println("Width: ${box1.width}")
+
+    box1.open()
+    box1.fillContents()
+}
